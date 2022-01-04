68b328ec3bda397f3be6a293894a8f64806de23b
Practice Golang <practice.golang@gmail.com>
2022-01-04 17:47:17 +0900

    Add test - logging

---
+++ logging/logging_test.go
@@ -0,0 +1,65 @@
+package logging
+
+import (
+	"io/ioutil"
+	"os"
+	"router-practice/variable"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_SetupLogger(t *testing.T) {
+	tests := []struct {
+		name string
+		want []byte
+	}{
+		{
+			name: "Test_SetupLogger",
+			want: []byte(`{"level":"info","message":"hello world"}` + "\n"),
+		},
+	}
+
+	fname := variable.ProgramName + "-" + time.Now().Format("20060102") + ".log"
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			SetupLogger()
+
+			Object.Info().Msg("hello world")
+			data, _ := ioutil.ReadFile(fname)
+
+			F.Close()
+			os.Remove(fname)
+
+			require.Equal(t, tt.want, data, "Not equal")
+		})
+	}
+}
+
+func Test_RenewLogger(t *testing.T) {
+	tests := []struct {
+		name string
+		want []byte
+	}{
+		{
+			name: "Test_SetupLogger",
+			want: []byte(`{"level":"info","message":"hello world"}` + "\n"),
+		},
+	}
+
+	fname := variable.ProgramName + "-" + time.Now().Format("20060102") + ".log"
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			RenewLogger()
+
+			Object.Info().Msg("hello world")
+			data, _ := ioutil.ReadFile(fname)
+
+			F.Close()
+			os.Remove(fname)
+
+			require.Equal(t, tt.want, data, "Not equal")
+		})
+	}
+}
