734d60244f4fd427bcd0f35b2055aaa41938764f
Practice Golang <practice.golang@gmail.com>
2022-01-16 23:51:54 +0900

    edit file chk

--- util/util.go
+++ util/util.go
@@ -8,23 +8,19 @@ import (
 )
 
 func CheckFileExists(path string, isEmbed bool) (result bool) {
+	result = false
+
 	switch isEmbed {
 	case true:
 		ef, err := fs.Stat(router.Content, path)
 		if err == nil && ef != nil {
 			result = true
-		} else {
-			result = false
 		}
-		break
 	case false:
 		f, err := os.Stat(path)
 		if err == nil && f != nil {
 			result = true
-		} else {
-			result = false
 		}
-		break
 	}
 
 	return
